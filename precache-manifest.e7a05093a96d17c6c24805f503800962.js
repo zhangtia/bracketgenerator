9659e6027f3ea8ffe31044400e573ec040ee8c79
zhangtia <zhangtia@umich.edu>
2019-11-20 18:05:38 -0500

    Updates

---
+++ precache-manifest.e7a05093a96d17c6c24805f503800962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e914e5436c25525fd3317c4f64cf2e9",
+    "url": "/bracketgenerator/index.html"
+  },
+  {
+    "revision": "2e12caa07fb8a3faa8f8",
+    "url": "/bracketgenerator/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "403174f20810ad95af38",
+    "url": "/bracketgenerator/static/js/2.188c2de0.chunk.js"
+  },
+  {
+    "revision": "2e12caa07fb8a3faa8f8",
+    "url": "/bracketgenerator/static/js/main.00fe2206.chunk.js"
+  },
+  {
+    "revision": "5a01ab726e4efeb9e11f",
+    "url": "/bracketgenerator/static/js/runtime-main.952d562c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bracketgenerator/static/media/logo.25bf045c.svg"
+  }
+]);
